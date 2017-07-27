96ec9170c2c0bc7d97de4bef4d6272d3c3ede743
Philippe Bachilli <bachilli@gmail.com>
2017-07-27 20:47:50 -0300

    change

--- invite.js
+++ invite.js
@@ -1,1 +1,8 @@
-"var geekdashboard = document.getElementsByClassName('uiButton _1sm'); for(var invite=0; invite<geekdashboard.length;invite++) { (function(invite) {        setTimeout(function(){            geekdashboard[invite].click();        }, 1000 + (3000 * invite));    })(invite); }" 
+var buttons = document.getElementsByClassName('uiButton _1sm');
+for(var invite=0; invite<buttons.length;invite++) {
+	(function(invite) {
+       setTimeout(function(){
+           buttons[invite].click();
+       }, 1000 + (3000 * invite));
+   })(invite);
+}
